1bae5c276cf4f2d93a9b3fb545dfd3af1f2e1ca7
sebokdas1 <sebok.das.ff@gmail.com>
2024-09-21 23:48:58 +0600

    set user to database by hashing password

--- src/app/(authentication)/sign-up/api/route.js
+++ src/app/(authentication)/sign-up/api/route.js
@@ -1,4 +1,6 @@
 import { connecDB } from "@/lib/connectDB";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
 export const POST = async (request) => {
   const newUser = await request.json();
   try {
@@ -6,14 +8,26 @@ export const POST = async (request) => {
     const userCollection = db.collection("users");
     const exist = await userCollection.findOne({ email: newUser.email });
     if (exist) {
-      return Response.json(
+      return NextResponse.json(
         { message: "user already have Registered" },
         { status: 304 }
       );
+    } else {
+      const hashedPassword = bcrypt.hashSync(newUser.password, 14);
+      const resp = await userCollection.insertOne({
+        ...newUser,
+        password: hashedPassword,
+      });
+
+      return NextResponse.json(
+        { message: "Succesfully Register" },
+        { status: 200 }
+      );
     }
-    const resp = await userCollection.insertOne(newUser);
-    return Response.json({ message: "Succesfully Register" }, { status: 200 });
   } catch (error) {
-    return Response.json({ message: "Something went wrong" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
   }
 };
