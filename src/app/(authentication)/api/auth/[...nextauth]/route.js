3010f8250a1c7704c2c28e80db65861fba479332
sebokdas1 <sebok.das.ff@gmail.com>
2024-09-22 01:08:51 +0600

    user matching

--- src/app/(authentication)/api/auth/[...nextauth]/route.js
+++ src/app/(authentication)/api/auth/[...nextauth]/route.js
@@ -1,4 +1,6 @@
+import { connecDB } from "@/lib/connectDB";
 import NextAuth from "next-auth/next";
+import bcrypt from "bcrypt";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
@@ -13,7 +15,23 @@ const handler = NextAuth({
         password: {},
       },
       async authorize(credentials) {
-        return true;
+        const { email, password } = credentials;
+        if (!email || !password) {
+          return null;
+        }
+        const db = await connecDB();
+        const currentUser = await db.collection("users").findOne({ email });
+        if (!currentUser) {
+          return null;
+        }
+        const passwordMatch = bcrypt.compareSync(
+          password,
+          currentUser.password
+        );
+        if (!passwordMatch) {
+          return null;
+        }
+        return currentUser;
       },
     }),
   ],
