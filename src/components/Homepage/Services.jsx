b787a259a8680c8c546fdc62303f0c86bdcc9a1c
sebokdas1 <sebok.das.ff@gmail.com>
2024-09-22 21:53:28 +0600

    get services from database and displayed

--- src/components/Homepage/Services.jsx
+++ src/components/Homepage/Services.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { services } from "@/lib/services";
 import ServiceCard from "../Card/ServiceCard";
 
-const Services = () => {
+const getServices = async () => {
+  const services = (
+    await fetch("http://localhost:3000/services/api/get-all")
+  ).json();
+  return services;
+};
+
+const Services = async () => {
+  const { services } = await getServices();
   return (
     <div className="text-slate-800 min-h-screen">
       <div className="text-center container mx-auto">
@@ -15,7 +22,7 @@ const Services = () => {
         </p>
       </div>
       <div className="container mx-auto mt-12 grid grid-cols-1 lg:grid-cols-3 gap-6">
-        {services.map((service) => (
+        {services?.map((service) => (
           <ServiceCard service={service} key={service._id} />
         ))}
       </div>
