d144f2ac999f9f64def68632bb2d002b522ae2e1
sebokdas1 <sebok.das.ff@gmail.com>
2024-09-23 19:48:34 +0600

    my bookings api make

---
+++ src/app/(server)/my-bookings/api/[email]/route.js
@@ -0,0 +1,14 @@
+import { connectDB } from "@/lib/connectDB";
+
+export const GET = async (request, { params }) => {
+  const db = await connectDB();
+  const bookingsCollection = db.collection("bookings");
+  try {
+    const bookings = await bookingsCollection
+      .find({ email: params.email })
+      .toArray();
+    return Response.json({ bookings });
+  } catch (error) {
+    console.log(error);
+  }
+};
