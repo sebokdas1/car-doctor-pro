681e66e9141a850dd4e81ab2d4f6af5f7a79e80f
sebokdas1 <sebok.das.ff@gmail.com>
2024-10-04 18:27:59 +0600

    product card

--- src/components/Card/ProductCard.jsx
+++ src/components/Card/ProductCard.jsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FaStar } from "react-icons/fa";
 import React from "react";
 
-const ProductCard = () => {
-  return <div></div>;
+const ProductCard = ({ product }) => {
+  return (
+    <div className="card bg-base-100 w-96 shadow-xl">
+      <figure className="px-6 bg-[#f3f3f3] py-6">
+        <Image
+          height={153}
+          width={156}
+          alt={product?.title}
+          src={product?.img}
+          className="rounded"
+        />
+      </figure>
+      <div className="card-body items-center text-center">
+        <small className="card-title text-yellow-400">
+          <FaStar />
+          <FaStar />
+          <FaStar />
+          <FaStar />
+          <FaStar />
+        </small>
+        <p>{product?.title}</p>
+        <div className="card-actions flex justify-between items-center">
+          <p className="text-[#FF3811]">${product?.price}</p>{" "}
+          <button className="btn  btn-outline">Add to Card</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
