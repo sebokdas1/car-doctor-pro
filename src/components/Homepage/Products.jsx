0468a15a8f62c36368b75a348afac00c691def59
sebokdas1 <sebok.das.ff@gmail.com>
2024-10-04 17:44:00 +0600

    products section complete

--- src/components/Homepage/Products.jsx
+++ src/components/Homepage/Products.jsx
@@ -1,6 +1,10 @@
+import { getProducts } from "@/lib/getServices";
 import React from "react";
+import ProductCard from "../Card/ProductCard";
 
-const Products = () => {
+const Products = async () => {
+  const { products } = await getProducts();
+  console.log(products);
   return (
     <div className="text-slate-800 mt-6">
       <div className="text-center container mx-auto">
@@ -12,11 +16,11 @@ const Products = () => {
           words which dont look even slightly believable.
         </p>
       </div>
-      {/* <div className="container mx-auto mt-12 grid grid-cols-1 lg:grid-cols-3 gap-6">
-        {services?.map((service) => (
-          <ServiceCard service={service} key={service._id} />
+      <div className="container mx-auto mt-12 grid grid-cols-1 lg:grid-cols-3 gap-6">
+        {products?.map((product) => (
+          <ProductCard product={product} key={product._id} />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 };
