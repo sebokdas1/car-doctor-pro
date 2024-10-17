d2189f87761bbcc35014a5b8e30ebe5fa3b730c0
sebokdas1 <sebok.das.ff@gmail.com>
2024-10-17 23:29:20 +0600

    remove unused things

--- src/app/(server)/sent-email/route.js
+++ src/app/(server)/sent-email/route.js
@@ -3,7 +3,6 @@ import nodemailer from "nodemailer";
 
 export const POST = async (request) => {
   const data = await request.json();
-  console.log(request);
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -14,17 +13,19 @@ export const POST = async (request) => {
 
   try {
     await transporter.sendMail({
-      from: process.env.DOCTOR_PRO_NODEMAILER_EMAIL,
-      to: data.email,
+      from: {
+        name: "Car Doctor Pro",
+        address: process.env.DOCTOR_PRO_NODEMAILER_EMAIL,
+      },
+      to: [data.email],
       subject: data.subject,
       text: data.text,
     });
     return NextResponse.json({ message: "Email sent" }, { status: 200 });
   } catch (error) {
-    console.log(error);
     return NextResponse.json(
       { message: "Error sending email" },
-      { data: data },
+      { error: error.message },
       { status: 400 }
     );
   }
