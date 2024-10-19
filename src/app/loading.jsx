36fbf04c2c9aa1f31ed1edc24af9bafbf82b33e7
sebokdas1 <sebok.das.ff@gmail.com>
2024-10-19 20:32:48 +0600

    latest

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,7 +4,6 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="h-screen text-5xl flex justify-center items-center">
-      {/* <span className="loading loading-infinity loading-lg"></span> */}
       <Image
         src={"/assets/images/loading-loader.gif"}
         width={460}
--- src/middleware.js
+++ src/middleware.js
@@ -6,14 +6,14 @@ export const middleware = async (request) => {
   const token = cookies(request).get("__Secure-next-auth.session-token");
   // For development token
   // const token = cookies(request).get("next-auth.session-token");
-  const pathname = request.nextUrl.pathname;
+  const pathname = await request?.nextUrl.pathname;
   if (pathname.includes("api")) {
     return NextResponse.next();
   }
 
   if (!token) {
     return NextResponse.redirect(
-      new URL(`/log-in?redirect=${pathname}`, request.url)
+      new URL(`/log-in?redirect=${pathname}`, request?.url)
     );
   }
   return NextResponse.next();
