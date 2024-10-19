875329df2425b0865dd084e836c67ba3913ca19e
sebokdas1 <sebok.das.ff@gmail.com>
2024-10-19 20:43:33 +0600

    changes at authprovider

--- src/services/AuthProvider.jsx
+++ src/services/AuthProvider.jsx
@@ -1,3 +1,17 @@
+// "use client";
+// import { SessionProvider } from "next-auth/react";
+// import React, { Suspense } from "react";
+
+// const AuthProvider = ({ children }) => {
+//   return (
+//     <SessionProvider>
+//       <Suspense>{children}</Suspense>
+//     </SessionProvider>
+//   );
+// };
+
+// export default AuthProvider;
+
 "use client";
 import { SessionProvider } from "next-auth/react";
 import React, { Suspense } from "react";
@@ -5,7 +19,7 @@ import React, { Suspense } from "react";
 const AuthProvider = ({ children }) => {
   return (
     <SessionProvider>
-      <Suspense>{children}</Suspense>
+      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
     </SessionProvider>
   );
 };
