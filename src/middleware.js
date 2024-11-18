8608fef3936e7daedd9d423bd63317c3b2e66afe
sebokdas1 <sebok.das.ff@gmail.com>
2024-11-18 19:49:17 +0600

    get token detail using getToken

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,17 @@
-import { cookies } from "next/headers";
+import { getToken } from "next-auth/jwt";
+// import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export const middleware = async (request) => {
-  const token = cookies(request).get(process.env.DOCTOR_PRO_COOKIE_TOKEN);
+  const token = await getToken({
+    req: request,
+    secret: process.env.DOCTOR_PRO_AUTH_SECRET,
+  });
+  // const token = cookies(request).get(process.env.DOCTOR_PRO_COOKIE_TOKEN);
   const pathname = await request?.nextUrl.pathname;
   if (pathname.includes("api")) {
     return NextResponse.next();
   }
-
   if (!token) {
     return NextResponse.redirect(
       new URL(`/log-in?redirect=${pathname}`, request.url)
@@ -18,10 +22,5 @@ export const middleware = async (request) => {
 };
 
 export const config = {
-  matcher: [
-    "/my-bookings/:path*",
-    "/admin/api/:path*",
-    "/checkout/:path*",
-    "/my-cart/:path*",
-  ],
+  matcher: ["/my-bookings/:path*", "/checkout/:path*", "/my-cart/:path*"],
 };
